a561f37acbaa80208812335182c42cf9dfc174a0
nhunht <nhtn0311@gmail.com>
2022-06-26 22:36:03 +0700

    Single Page Applications Part 2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -51,23 +51,7 @@ const Menu = (props) => {
       <div className="row">{menu}</div>
     </div>
   );*/
-  return (
-    <div className="container">
-      <div className="row">
-        <Breadcrumb>
-          <BreadcrumbItem>
-            <Link to="/home">Home</Link>
-          </BreadcrumbItem>
-          <BreadcrumbItem active>Menu</BreadcrumbItem>
-        </Breadcrumb>
-        <div className="col-12">
-          <h3>Menu</h3>
-          <hr />
-        </div>
-      </div>
-      <div className="row">{menu}</div>
-    </div>
-  );
+  
 };
 
 export default Menu;
