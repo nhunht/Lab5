61651c7b5ca2a9644d720c41095039c1b359a728
nhunht <nhtn0311@gmail.com>
2022-07-18 19:37:04 +0700

    Lab8

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,59 +1,63 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle} from 'reactstrap';
 import { Loading } from './LoadingComponent';
-import { baseUrl } from '../shared/baseUrl';
 import { FadeTransform } from 'react-animation-components';
-
-function RenderCard({item, isLoading, errMess}) {
-    
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle
+} from 'reactstrap';
+import { baseUrl } from '../shared/baseUrl';
+const CardImgFixHeight = {
+    height: '20rem',
+    minHeight: '20rem',
+    maxHeight: '20rem'
+}
+const cardImgFix15 = {
+    height: '20rem',
+}
+function RenderCard({ item, isLoading, errMess }) {
+    console.log(item)
     if (isLoading) {
-        return(
+        return (
             <Loading />
         );
     }
     else if (errMess) {
-        return(
+        return (
             <h4>{errMess}</h4>
         );
     }
-    else 
-        return(
+    else
+        return (
             <FadeTransform
                 in
                 transformProps={{
                     exitTransform: 'scale(0.5) translateY(-50%)'
                 }}>
                 <Card>
-                    <CardImg src={baseUrl + item.image} alt={item.name} />
-                    <CardBody>
-                    <CardTitle>{item.name}</CardTitle>
-                    {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
-                    <CardText>{item.description}</CardText>
+                    <CardImg style={CardImgFixHeight} src={item.image} alt={item.name} />
+                    <CardBody style={cardImgFix15}>
+                        <CardTitle>{item.name}</CardTitle>
+                        {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
+                        <CardText>{item.description}</CardText>
                     </CardBody>
                 </Card>
             </FadeTransform>
         );
+
 }
 
 function Home(props) {
-    return(
+    return (
         <div className="container">
             <div className="row align-items-start">
                 <div className="col-12 col-md m-1">
-                    {/* <RenderCard item={props.dish} /> */}
-                    <RenderCard item={props.dish} 
-                        isLoading={props.dishesLoading} 
-                        errMess={props.dishesErrMess} />
+                    <RenderCard item={props.dish} isLoading={props.dishesLoading} errMess={props.dishesErrMess} />
                 </div>
                 <div className="col-12 col-md m-1">
-                    {/* <RenderCard item={props.promotion} /> */}
-                    <RenderCard item={props.promotion} 
-                        isLoading={props.promoLoading} 
-                        errMess={props.promoErrMess} />
+                    <RenderCard item={props.promotion} isLoading={props.promoLoading} errMess={props.promoErrMess} />
                 </div>
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.leader} />
+                    <RenderCard item={props.leader} isLoading={props.leaderLoading} errMess={props.leaderErrMess} />
                 </div>
             </div>
         </div>
